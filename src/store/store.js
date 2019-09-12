7725291357682ddaab1bd4cde2990ef68ed64d7c
dbabu <doggaananda.babu@publicssapient.com>
2019-09-12 14:11:38 +0530

    removed redux tool plugin for browsers

--- src/store/store.js
+++ src/store/store.js
@@ -10,10 +10,7 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleWare),
-    window.navigator.userAgent.includes('Chrome')?
-     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-     :compose
+    applyMiddleware(...middleWare)
   )
 );
 
